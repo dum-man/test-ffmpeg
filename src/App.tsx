b0db341c5fca3ba46a74e3cc808f87781e890620
Duman <duman.abdrakhmanov@outlook.com>
2025-05-17 16:16:04 +0500

    adds mobile safari

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,26 @@ const isWebview = (userAgent: string) => {
   return /webview|wv|ip((?!.*Safari)|(?=.*like Safari))/i.test(userAgent);
 };
 
+function isMobileSafari() {
+  const ua = navigator.userAgent || "";
+  const isIOS = /iP(hone|od|ad)/.test(ua);
+  const isWebkit = /WebKit/.test(ua);
+  const isChrome = /CriOS/.test(ua); // Chrome on iOS
+  const isFirefox = /FxiOS/.test(ua); // Firefox on iOS
+
+  return isIOS && isWebkit && !isChrome && !isFirefox;
+}
+
 function App() {
   if (isWebview(window.navigator.userAgent)) {
     return <h1>WebView!</h1>;
   }
 
-  return <h1>{window.navigator.userAgent}</h1>;
+  if (isMobileSafari()) {
+    return <h1>Mobile Safari IOS</h1>;
+  }
+
+  return <p>{window.navigator.userAgent}</p>;
 }
 
 export default App;
